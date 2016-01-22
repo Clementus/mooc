ecf7c12a039691ff0bc31cbdfd84a8c31c334a07
Erozbliz <erozbliz@hotmail.com>
2016-01-22 17:34:19 +0100

    maj

--- production/inscription.php
+++ production/inscription.php
@@ -1,6 +1,7 @@
 <?php
 include "connect.inc.php";  /// Connection bdd
 
+echo "incription.php";
  if (isset($_POST['firstName'])) {
  	echo $_POST['firstName'];
  	echo "-->".md5($_POST['firstName']);
